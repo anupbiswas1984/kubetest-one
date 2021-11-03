784d50be283d5025cdf18652dfda282cac2032d4
anup <anupbiswas1984@gmail.com>
2021-11-03 14:18:27 +0530

    first commit

---
+++ src/main/java/com/kubetestone/KubetestOneApplication.java
@@ -0,0 +1,13 @@
+package com.kubetestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubetestOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubetestOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubetestone/KubetestOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubetestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubetestOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
